7f8232c1430fd72c32624ec47a78deca081dafcc
nlom0218 <nlom0218@naver.com>
2022-12-16 21:16:05 +0900

    feat: 원하는 기능을 입력하기

--- src/controllers/Controller.js
+++ src/controllers/Controller.js
@@ -9,7 +9,27 @@ class Controller {
     InputView.readAppFunction((appFn) => this.handleAppFunction(appFn));
   }
 
-  handleAppFunction(appFn) {}
+  handleAppFunction(appFn) {
+    if (appFn === '1') return this.pairMatching();
+
+    if (appFn === '2') return this.pairLookup();
+
+    if (appFn === '3') return this.pairReset();
+
+    console.log('앱을 종료합니다.');
+  }
+
+  pairMatching() {
+    console.log('페어 매칭을 합니다.');
+  }
+
+  pairLookup() {
+    console.log('페어 조회를 합니다.');
+  }
+
+  pairReset() {
+    console.log('페어 초기화를 합니다.');
+  }
 }
 
 module.exports = Controller;
